4ff7f429319de5c6b8b1b66a717568d16722742a
Burabyo <j.burabyo@alustudent.com>
2025-02-05 15:00:29 +0200

    second

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+if (argscount === 0){
+  console.log("No argument");
+}
+else if(argscount=== 1) {
+  console.log("Argument found");
+}
+else {
+  console.log("Arguments found");
+}
+  
