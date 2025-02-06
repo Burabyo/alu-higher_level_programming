a1238137c2848111020b6878095dbd5d27fb164c
Burabyo <j.burabyo@alustudent.com>
2025-02-06 11:31:37 +0200

    changes

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-if (argscount === 0){
-  console.log("No argument");
+
+if (!process.argv[2]) {
+  console.log('No argument');
+} else if ((process.argv[2]) && (!process.argv[3])) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-else if(argscount=== 1) {
-  console.log("Argument found");
-}
-else {
-  console.log("Arguments found");
-}
+
+
   
