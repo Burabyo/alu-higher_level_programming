0d676cb6fa4ff1a8e211843d3a54816fd74a08c8
Burabyo <j.burabyo@alustudent.com>
2025-02-20 12:31:24 +0200

    scrap

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
-const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+
+const request = require('fs');
+
+const filePath = process.argv[2];
+
+request.readFile(filePath, function (err, data) {
+  if (err) console.log(err);
+  const content = data;
+  console.log(content.toString());
 });
