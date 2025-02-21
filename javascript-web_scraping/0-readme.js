ef9dee08ca8fb659cba03674d7fde6ee35091352
Burabyo <j.burabyo@alustudent.com>
2025-02-21 23:24:18 +0200

    change

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
+const myFile = process.argv[2];
+const fs = require('fs');
+fs.readFile(myFile, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-const request = require('fs');
-
-const filePath = process.argv[2];
-
-request.readFile(filePath, function (err, data) {
-  if (err) console.log(err);
-  const content = data;
-  console.log(content.toString());
+  console.log(data);
 });
